d23765d19b576183b1cdfac0e9240ec8ce6380e8
Olena Jivilo <lenario22@ukr.net>
2025-01-29 12:00:48 +0200

    Add files via upload

---
+++ src/data/quotes.js
@@ -0,0 +1,134 @@
+const quotes = [
+    {
+      id: 'c718475e',
+      text: 'The only way to do great work is to love what you do',
+      author: 'Steve Jobs',
+    },
+    {
+      id: '3452d8aa',
+      text: 'Innovation distinguishes between a leader and a follower',
+      author: 'Steve Jobs',
+    },
+    {
+      id: '56487b49',
+      text: "Your time is limited, so don't waste it living someone else's life",
+      author: 'Steve Jobs',
+    },
+    {
+      id: '1e3736f8',
+      text: 'Strive not to be a success, but rather to be of value',
+      author: 'Albert Einstein',
+    },
+    {
+      id: '2520d4da',
+      text: 'The only thing we have to fear is fear itself',
+      author: 'Franklin D. Roosevelt',
+    },
+    {
+      id: '0446d47e',
+      text: 'Life is what happens when you’re busy making other plans',
+      author: 'John Lennon',
+    },
+    {
+      id: 'ea08d995',
+      text: 'Success is not final, failure is not fatal: It is the courage to continue that counts',
+      author: 'Winston Churchill',
+    },
+    {
+      id: '9ac6bee8',
+      text: 'The greatest glory in living lies not in never falling, but in rising every time we fall',
+      author: 'Nelson Mandela',
+    },
+    {
+      id: '05370aeb',
+      text: 'The future belongs to those who believe in the beauty of their dreams',
+      author: 'Eleanor Roosevelt',
+    },
+    {
+      id: '423c35bb',
+      text: 'You miss 100% of the shots you don’t take',
+      author: 'Wayne Gretzky',
+    },
+    {
+      id: 'a12fbd4e',
+      text: 'Be so good they can’t ignore you',
+      author: 'Steve Martin',
+  },
+  {
+      id: 'b9fd18ec',
+      text: 'What we think, we become',
+      author: 'Buddha',
+  },
+  {
+      id: 'da71c3af',
+      text: 'All our dreams can come true, if we have the courage to pursue them',
+      author: 'Walt Disney',
+  },
+  {
+      id: 'aefc37b9',
+      text: 'It does not matter how slowly you go as long as you do not stop',
+      author: 'Confucius',
+  },
+  {
+      id: 'ed3af2b4',
+      text: 'Everything you can imagine is real',
+      author: 'Pablo Picasso',
+  },
+  {
+      id: '71a9bfcd',
+      text: 'Whatever you are, be a good one',
+      author: 'Abraham Lincoln',
+  },
+  {
+      id: '2f3e91bc',
+      text: 'Act as if what you do makes a difference. It does',
+      author: 'William James',
+  },
+  {
+      id: 'c8e42bf9',
+      text: 'The only way to do great work is to love what you do',
+      author: 'Steve Jobs',
+  },
+  {
+      id: 'd312be47',
+      text: 'If you want to lift yourself up, lift up someone else',
+      author: 'Booker T. Washington',
+  },
+  {
+      id: 'e41acf92',
+      text: 'Strive not to be a success, but rather to be of value',
+      author: 'Albert Einstein',
+  },
+  {
+      id: 'bc4f8d32',
+      text: 'The best way to predict the future is to invent it',
+      author: 'Alan Kay',
+  },
+  {
+      id: 'a13f7b29',
+      text: 'Do one thing every day that scares you',
+      author: 'Eleanor Roosevelt',
+  },
+  {
+      id: 'f9d3ab4c',
+      text: 'Dream big and dare to fail',
+      author: 'Norman Vaughan',
+  },
+  {
+      id: 'a421cf87',
+      text: 'The power of imagination makes us infinite',
+      author: 'John Muir',
+  },
+  {
+      id: '23f19ae4',
+      text: 'Don’t watch the clock; do what it does. Keep going',
+      author: 'Sam Levenson',
+  },
+  {
+      id: 'a8f34d92',
+      text: 'You are never too old to set another goal or to dream a new dream',
+      author: 'C.S. Lewis',
+  },
+  ]
+
+  export default quotes;
